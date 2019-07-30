082d3159c6a6f12e06010da50538258f484360f4
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-07-30 14:49:56 +0300

    subsys: add module

---
+++ yomiel/subsys/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# jomiel-yomiel
+#
+# Copyright
+#  2019 Toni Gündoğdu
+#
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+"""TODO."""
+
+
+def init():
+    """Initiates the application subsystems."""
+
+    from yomiel.subsys import log
+    log.init()
+
+    from yomiel import lg
+    lg().info('all subsystems initiated')
+
+
+# vim: set ts=4 sw=4 tw=72 expandtab:
