0615a0265a61da05fc679caf9d04bf2e98402aa7
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-07-30 14:46:58 +0300

    __main__.py: add file

---
+++ yomiel/__main__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# jomiel-yomiel
+#
+# Copyright
+#  2019 Toni Gündoğdu
+#
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+"""TODO."""
+
+if __name__ == '__main__':
+    from sys import path
+    path.insert(0, '.')
+
+    from yomiel import main
+    main()
+
+# vim: set ts=4 sw=4 tw=72 expandtab:
