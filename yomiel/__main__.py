42e72957e71f057fac6237876c303a3acb1a4a18
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-11-07 16:08:27 +0200

    yomiel/__main__.py: make style changes according to "black" code formatter

--- yomiel/__main__.py
+++ yomiel/__main__.py
@@ -10,11 +10,13 @@
 #
 """TODO."""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sys import path
-    path.insert(0, '.')
+
+    path.insert(0, ".")
 
     from yomiel import main
+
     main()
 
 # vim: set ts=4 sw=4 tw=72 expandtab:
