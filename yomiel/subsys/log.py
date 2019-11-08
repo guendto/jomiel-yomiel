055b91b5895bfb6677847cfca6dc3811edbf30a8
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-11-08 09:46:40 +0200

    subsys/log: remove pylint (disable) comment

--- yomiel/subsys/log.py
+++ yomiel/subsys/log.py
@@ -13,7 +13,7 @@
 
 def init():
     """Initiates the logging subsystem."""
-    from yomiel.cache import logger_paths, opts  # pylint: disable=E0611
+    from yomiel.cache import logger_paths, opts
     from yomiel.kore.log import log_init
 
     (logger_file, logger_idents) = log_init(logger_paths)
